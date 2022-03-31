94d4048d0d30e551cd988866559bc9ba9fcbe719
practice-stack1 <vadim3d2011@gmail.com>
2022-03-31 11:05:31 +0300

    prod 1.2.0

--- src/js/components/UI/Input.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Input = (props) => {
-  return (
-    <input {...props} aria-label="default input example" style={{marginBottom: '20px'}}></input>
-  )
-}
-
-export default Input
